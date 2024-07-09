b92b527154094a28b4fb05fb692b9b589f50c208
Jorge Bayuelo <jbayuelo@firsttube.com>
2024-07-09 11:44:10 -0500

    Learn about variables

--- main.go
+++ main.go
@@ -2,6 +2,44 @@ package main
 
 import "fmt"
 
+const PI float64 = 3.14
+
+// func main() {
+// 	fmt.Println("Hello World!")
+// }
+
+//Declaring Variables//
+
+// func main() {
+// 	var greeting string = "Hello World"
+// 	fmt.Println(greeting)
+// }
+
+//Print variable and string//
+
+// func main() {
+// 	var name string = "KodeKloud"
+// 	var user string = "Harry"
+
+// 	fmt.Print("Welcome to ", name, ", ",user)
+// }
+
+//Printing on newline//
+
+// func main() {
+// 	var name string = "KodeKloud"
+// 	var user string = "Harry"
+
+// 	fmt.Println(name)
+// 	fmt.Println(user)
+// }
+
 func main() {
-	fmt.Println("Hello World!")
+	var radius float64 = 5.154
+	var area float64
+
+	area = PI * radius * radius
+	fmt.Printf("Radius: %.2f \nPI:%.1f \n", radius, PI)
+	fmt.Printf("Area of Circle is : %f", area)
+	fmt.Println("Thank You")
 }
