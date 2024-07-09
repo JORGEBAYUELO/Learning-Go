19fe84dc8b0d4cc53aeb10b421ecdbbd35056ed3
Jorge Bayuelo <jbayuelo@firsttube.com>
2024-07-09 16:47:56 -0500

    Arrays & Slice

--- main.go
+++ main.go
@@ -57,7 +57,107 @@ const PI float64 = 3.14
 // 	fmt.Println((x+y) >> 2)
 // }
 
+// func main() {
+// 	var x, y int = 100,90
+// 	fmt.Println(!(((x+y) >> 2 ) == 47))
+// }
+
+//If, Else, Else-if, Switch//
+
+// func main() {
+// 	var a, b = 100, 5
+// 	switch {
+// 	case a/b == 10:
+// 		fmt.Println("10")
+// 	case a/b == 20:
+// 		fmt.Println("20")
+// 	case a/b == 10:
+// 		fmt.Println("30")
+// 	default:
+// 		fmt.Println("default") 
+		
+// 	}
+// }
+
+// func main() {
+// 	var a, b = 100, 5
+// 	switch a {
+// 	case a/b == 10:
+// 		fmt.Println("10")
+// 	case a/b == 20:
+// 		fmt.Println("20")
+// 	case a/b == 10:
+// 		fmt.Println("30")
+// 	default:
+// 		fmt.Println("default") 
+		
+// 	}
+// }
+
+//Arrays & Slice//
+
+// func main() {
+// 	arr := []int{-1, -2}
+// 	for _, value := range arr {
+// 		fmt.Println(value)
+// 	}
+// }
+
+// func main() {
+// 	arr := [5]string{"a", "b", "c", "d", "e"}
+// 	slice := arr[:4]
+// 	fmt.Println(arr)
+// 	fmt.Println(slice)
+// 	slice[1] = "x"
+// 	fmt.Println(arr)
+// 	fmt.Println(slice)
+// }
+
+// func main() {
+// 	arr := [5]int{10, 20, 90, 70, 60}
+// 	slice := arr[:3]
+// 	fmt.Println(cap(slice))
+// 	new_slice := append(slice, 100, 200)
+// 	fmt.Println(cap(new_slice))
+// }
+
+// func main() {
+// 	arr := [5]int{10, 20, 90, 70, 60}
+// 	slice := arr[:3]
+// 	fmt.Println(cap(slice))
+
+// 	slice_2 := make([]int, 5, 20)
+// 	new_slice := append(slice, slice_2...)
+// 	fmt.Println(cap(new_slice))
+
+// }
+
+// func main() {
+// 	arr := [5]int{10, 20, 90, 70, 60}
+// 	slice := append(arr[:2], arr[3:])
+// 	fmt.Println(slice)
+// }
+
+// func main() {
+// 	arr := []int{10, 20, 90, 70, 60}
+// 	slice := make([]int, 10)
+// 	num := copy(slice, arr)
+// 	fmt.Println(slice)
+// 	fmt.Println(num)
+// }
+
+// func main() {
+// 	arr := []int{10, 20, 90, 70, 60}
+// 	slice := make([]int, 10)
+// 	copy(slice, arr)
+// 	slice[1] = 1000
+// 	fmt.Println(arr)
+// 	fmt.Println(slice)
+// }
+
 func main() {
-	var x, y int = 100,90
-	fmt.Println(!(((x+y) >> 2 ) == 47))
+	arr := [10]int{10, 20}
+	slice := arr[2:8]
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
 }
