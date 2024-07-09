317c06206f32fc79ff47721bdbeb791f485a01f7
Jorge Bayuelo <jbayuelo@firsttube.com>
2024-07-09 13:16:23 -0500

    Bitwise operators

--- main.go
+++ main.go
@@ -34,12 +34,30 @@ const PI float64 = 3.14
 // 	fmt.Println(user)
 // }
 
-func main() {
-	var radius float64 = 5.154
-	var area float64
+// func main() {
+// 	var radius float64 = 5.154
+// 	var area float64
 
-	area = PI * radius * radius
-	fmt.Printf("Radius: %.2f \nPI:%.1f \n", radius, PI)
-	fmt.Printf("Area of Circle is : %f", area)
-	fmt.Println("Thank You")
+// 	area = PI * radius * radius
+// 	fmt.Printf("Radius: %.2f \nPI:%.1f \n", radius, PI)
+// 	fmt.Printf("Area of Circle is : %f", area)
+// 	fmt.Println("Thank You")
+// }
+
+//Bitwise Operators//
+
+// func main() {
+// 	var x, y int = 100,90
+// 	fmt.Println(x & y)
+// 	fmt.Println(x | y)
+// }
+
+// func main() {
+// 	var x, y int = 100,90
+// 	fmt.Println((x+y) >> 2)
+// }
+
+func main() {
+	var x, y int = 100,90
+	fmt.Println(!(((x+y) >> 2 ) == 47))
 }
